236ac7dcd2b80e52bfb5b1fe39994ceffb9a68e3
Leonid Nikolaev <nikonor@gmail.com>
2025-09-14 18:10:54 +0300

    [+] validate input date as Monday

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use chrono::{Days, NaiveDate, ParseError};
+use chrono::{Datelike, Days, NaiveDate, ParseError, Weekday::Mon};
 use std::io::{Write, stderr};
-use std::ops::Add; // Modified import to include all needed Chrono traits
+use std::ops::Add;
 
 fn parse_date(s: String) -> Result<NaiveDate, ParseError> {
     match NaiveDate::parse_from_str(s.as_str(), "%Y-%m-%d") {
@@ -21,6 +21,7 @@ fn test_empty_date_string() {
     let empty_date_str = "".to_string();
     assert!(parse_date(empty_date_str).is_err());
 }
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 3 {
@@ -36,11 +37,17 @@ fn main() {
     let d = parse_date(args[1].to_string());
     match d {
         Err(e) => {
-            writeln!(stderr(), "wrong date {}::{}", args[1], e.to_string()).unwrap();
+            writeln!(stderr(), "Error::wrong date {}::{}", args[1], e.to_string()).unwrap();
             std::process::exit(1);
         }
-        Ok(_) => {}
+        Ok(d) => {
+            if d.weekday() != Mon {
+                writeln!(stderr(), "Error::{} is not monday", args[1]).unwrap();
+                std::process::exit(1);
+            }
+        }
     }
     let d = d.unwrap().add(Days::new(1));
     println!("d={}", d);
+    println!("d={}", d.weekday());
 }
